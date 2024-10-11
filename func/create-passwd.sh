d3cc377279a668704d0740436da954a981209a15
qvgz <i@qvgz.org>
2024-10-11 15:41:07 +0800

    重写支持 macOS

--- func/create-passwd.sh
+++ func/create-passwd.sh
@@ -11,21 +11,25 @@ function create_passwd(){
     echo "密码位数至少为 4 位，数字 大写字符 标点符号"
     exit 1
   fi
-  # 循环生成密码，直至生成符合要求密码
-  while : ; do
-    # tr -d 删除指定集 -c 反转
-    passwd="$(strings /dev/urandom | tr -dc '[:print:]'  | head -c$num)"
-    # 密码强度验证
-    # 必须含有 数字 大小写字符 标点符号
-    if [[ $(echo $passwd | sed 's/[[:digit:]]//') != "$passwd" && \
-    $(echo $passwd | sed 's/[[:lower:]]//') != "$passwd" && \
-    $(echo $passwd | sed 's/[[:upper:]]//') != "$passwd" && \
-    $(echo $passwd | sed 's/[[:punct:]]//') != "$passwd" && \
-    # 排除难终端分辨字符出现如 空格 等
-    $(echo $passwd | sed "s/[ 0oO1Ilz29gb6\"'\`]//") == "$passwd" ]] ; then
-      break
-    fi
-  done
+
+  # 定义字符集
+  local digits="23456789"
+  local lowercase="abcdefghijkmnpqrstuvwxyz"
+  local uppercase="ABCDEFGHJKMNPQRSTUVWXYZ"
+  local symbols="!@#$%^&*()-+=[]{}|;:,.<>?"
+  local all_chars="${digits}${lowercase}${uppercase}${symbols}"
+
+  # 确保每种字符至少出现一次
+  passwd="$(fold -w1 <<< "${digits}" | shuf -n1)$(fold -w1 <<< "${lowercase}"| shuf -n1)$(fold -w1 <<< "${uppercase}" | shuf -n1)$(fold -w1 <<< "${symbols}"| shuf -n1)"
+    
+  # 填充剩余长度
+  remaining_length=$((num - 4))
+  if (( remaining_length > 0 )); then
+      passwd+="$(fold -w1 <<< ${all_chars} | shuf -n"${remaining_length}" | tr -d '\n')"
+  fi
+  
+  # 打乱密码顺序
+  passwd="$(fold -w1 <<< ${passwd} | shuf | tr -d '\n')"
   echo $passwd
 }
 create_passwd $1

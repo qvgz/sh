57cade3e5fd21f9095ea950d4e9679bf3647bcbf
qvgz <i@qvgz.org>
2024-10-19 17:04:41 +0800

    优化 0 链接附件暂存路径与脚本性能

--- macos/obsidian-att-clean.sh
+++ macos/obsidian-att-clean.sh
@@ -6,21 +6,24 @@
 # 环境变量中 仓库路径 与 0 链接附件暂存路径 不为空
 [[ -z $OBSIDIAN_VAULT_PATH || -z $OBSIDIAN_TRASH_PATH ]] && exit 1
 # 附件默认存放路径为指定的附件文件夹 "归档/附件"
+vault_name="${OBSIDIAN_VAULT_PATH##*/}"
 md_path_str=$(find "$OBSIDIAN_VAULT_PATH" -not -path '*/.trash/*' -not -path '*/归档/附件/*' -type f -name '*.md' | awk '{printf("%s%c",$0,10)}')
 
 # md 中含有附件名提前退出
 function md_check_att (){
     att_path=$1  
     att_name=$(echo $att_path | awk -F/ '{printf("%s",$NF)}')
-    IFS=$'\n'
-    for md in $md_path_str;do
-        if [[ $(grep -c "${att_name}" "${md}") != 0 ]];then
-            return 0
-        fi
-    done
-    mv "$att_path" "${OBSIDIAN_TRASH_PATH}/$(date +%Y%m%d%H%M%S)-${att_name}"
+    num="$(grep -cr "$att_name" --exclude-dir={.obsidian,.trash,附件,} --include="*.md" "$OBSIDIAN_VAULT_PATH" | awk -F: '{sum+=$2} END {print sum}')"
+    if [[ "$num" == "0" ]];then
+        # 拼接 0 链接附件暂存路径
+        # $OBSIDIAN_TRASH_PATH/vault_name/归档/附件/
+        path="$(echo "$att_path" | awk -F${vault_name} '{print $2}')"
+        path="${OBSIDIAN_TRASH_PATH}/${vault_name}${path%/*}"
+        mkdir -p "$path" && mv "${att_path}" "${path}/${att_name}" 
+    fi
 }
 
+export vault_name
 export md_path_str
 export OBSIDIAN_TRASH_PATH
 export -f md_check_att

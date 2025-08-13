ca80bc6ccb9d2d2086a720df62a8c80040b49c75
qvgz <i@qvgz.org>
2025-08-13 16:53:29 +0800

    添加 rhel

--- install/docker.sh
+++ install/docker.sh
@@ -36,6 +36,18 @@ function centos_install(){
   sudo yum install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 }
 
+# rhel
+# https://docs.docker.com/engine/install/rhel/
+function rhel_install(){
+  sudo dnf remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine podman runc
+
+  sudo dnf -y install dnf-plugins-core
+  sudo dnf config-manager --add-repo https://${MORROR}/linux/$1/docker-ce.repo
+  sudo sed -i "s|download.docker.com|${MORROR}|g" /etc/yum.repos.d/docker-ce.repo
+
+  sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+}
+
 # arch
 # https://wiki.archlinux.org/title/Docker
 function arch_install(){
@@ -63,7 +75,11 @@ case  $distro in
     INSTALL="sudo apt install -y jq"
     debian_install
     ;;
-  "centos" | "almalinux")
+  "rhel" | "almalinux")
+    INSTALL="sudo yum install -y"
+    rhel_install "rhel"
+    ;;
+  "centos")
     INSTALL="sudo yum install -y"
     centos_install "centos"
     ;;
